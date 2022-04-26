c65743b1b8e96cc783163ca75f583b92ef1fa256
Gui-Dias05 <diasarielguilherme@gmail.com>
2022-04-26 19:19:17 -0300

    ativ prova prog atualizado

---
+++ php/menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg ">
+  <div >
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tabelas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="tabelaPessoaFisica.php">Tabela Pessoa Física</a></li>
+            <li><a class="dropdown-item" href="tabelaContaCorrente.php">Tabela Conta Corrente</a></li>
+            <li><a class="dropdown-item" href="tabelaContatos.php">Tabela Contatos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cadastros
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="cadPessoaFisica.php">Cadastro Pessoa Física</a></li>
+            <li><a class="dropdown-item" href="cadContaCorrente.php">Cadastro Conta Corrente</a></li>
+            <li><a class="dropdown-item" href="cadContatos.php">Cadastro Contatos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="operacao.php">Operação</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br>
+<script src="../bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
